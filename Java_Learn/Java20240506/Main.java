4c1abdcdd49b5fd06faefa6a125eabafd444e51e
SunJK <sunjk.cool@gmail.com>
2024-05-06 17:16:04 +0800

    create interface Person and implements student

--- Java_Learn/Java20240506/Main.java
+++ Java_Learn/Java20240506/Main.java
@@ -3,3 +3,27 @@ package Java_Learn.Java20240506;
 public class Main {
 
 }
+
+interface Person {
+    void run();
+
+    String getName();
+}
+
+class Student implements Person {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(this.name+"run");
+    }
+
+    @Override
+    public String getName(){
+        return this.name;
+    }
+}
